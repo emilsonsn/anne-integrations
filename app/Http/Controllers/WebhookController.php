e997058b39aa0c3c264b6476d5861a39ad1aa6f4
emilsonsn <emilsonsn2@gmail.com>
2025-03-14 18:02:16 -0300

    fix: ajusta mensagens dos eventos

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -88,10 +88,10 @@ class WebhookController extends Controller
     private function getMensagemPorTipo($tipo, $nome, $evento)
     {
         return match ((int) $tipo) {
-            2 => "Olá, $nome! Vimos que a sua inscrição no evento $evento foi confirmada com sucesso! Agora é só aguardar o grande dia chegar.\n\nPara saber mais detalhes sobre o evento e a sua inscrição, digite Confirmado ou confirmada que eu te conto tudo, até mais!",
-            3 => "Olá, $nome! Recebemos o seu pedido de cancelamento da inscrição no evento $evento. Lamentamos que não poderá participar desta vez.\n\nCaso mude de ideia ou precise de ajuda para se inscrever em outro evento, é só me avisar! Estamos à disposição. Mas se você quiser conversar com os nossos atendentes e organizadores, digite Atendimento",
-            7 => "Olá, $nome! Vimos que você efetuou o check-in no evento $evento. Que incrível!\n\nTemos várias atividades acontecendo durante o evento. Gostaria de receber atualizações sobre as melhores informações até o término? Então, digite a palavra CHEGUEI abaixo e aproveite o evento. Só digitar CHEGUEI",
-            12 => "Olá, $nome! Informamos que o estorno da sua compra foi processado com sucesso. O valor será devolvido conforme as políticas do seu método de pagamento.\n\nCaso tenha alguma dúvida ou precise de ajuda para outras compras, é só me avisar! Estamos aqui para ajudar. Digite Ajuda que te transferimos para o atendimento sobre dúvidas.",
+            2 => "Olá, $nome! Vimos que a sua inscrição no evento $evento foi confirmada com sucesso! Agora é só aguardar o grande dia chegar.\n\nPara saber mais detalhes sobre o evento e a sua inscrição, digite confirmado ou confirmada que eu te conto tudo, até mais!",
+            3 => "Olá, $nome! Recebemos o seu pedido de cancelamento da inscrição no evento $evento. Lamentamos que não poderá participar desta vez.\n\nCaso mude de ideia ou precise de ajuda para se inscrever em outro evento, é só me avisar! Estamos à disposição. Mas se você quiser conversar com os nossos atendentes e organizadores, digite atendimento",
+            7 => "Olá, $nome! Vimos que você efetuou o check-in no evento $evento. Que incrível!\n\nTemos várias atividades acontecendo durante o evento. Gostaria de receber atualizações sobre as melhores informações até o término? Então, digite a palavra cheguei abaixo e aproveite o evento. Só digitar cheguei",
+            12 => "Olá, $nome! Informamos que o estorno da sua compra foi processado com sucesso. O valor será devolvido conforme as políticas do seu método de pagamento.\n\nCaso tenha alguma dúvida ou precise de ajuda para outras compras, é só me avisar! Estamos aqui para ajudar. Digite ajuda que te transferimos para o atendimento sobre dúvidas. Só digitar ajuda",
             default => null
         };
     }
